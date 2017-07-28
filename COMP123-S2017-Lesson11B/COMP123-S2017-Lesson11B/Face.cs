fe6ad383cfec92221dc7bbf32d8ece679c3d4e6b
Harmanpreet Singh <Harmanpreet.preetsingh@gmail.com>
2017-07-27 22:27:47 -0400

    Created the Face and Suit enums

---
+++ COMP123-S2017-Lesson11B/COMP123-S2017-Lesson11B/Face.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Harmanpreet Singh
+ * Date: July 27, 2017
+ * Description: This is the Face enum
+ * Version: 0.1 - Created the Face enum
+ */
+
+namespace COMP123_S2017_Lesson11B
+{
+    public enum Face
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
--- COMP123-S2017-Lesson11B/COMP123-S2017-Lesson11B/Program.cs
+++ COMP123-S2017-Lesson11B/COMP123-S2017-Lesson11B/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Name: Harmanpreet Singh
+ * Date: July 27, 2017
+ * Description: This is a Demo for Class 11
+ * Version: 0.1 - Created the project
+ */
+
 namespace COMP123_S2017_Lesson11B
 {
     class Program
